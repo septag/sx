76ea0d2ac6b30ec8a382774204c6e23e1daf88cb
D4 <d4@thegrid.io>
2021-05-30 06:45:58 -0700

    expanded test-fiber a bit

--- tests/test-fiber.c
+++ tests/test-fiber.c
@@ -15,34 +15,45 @@ void fiber1_fn(sx_fiber_transfer transfer)
         transfer.user);    // Always put this to return to caller at the end of the callback
 }
 
+typedef struct coro_data {
+    sx_coro_context* ctx;
+    int frame;
+} coro_data;
+
 sx_coro_declare(wait_test)
 {
-    sx_coro_context* ctx = sx_coro_userdata();
+    coro_data* d = sx_coro_userdata();
     puts("fiber_wait_fn: Start");
 
     puts("fiber_wait_fn: Waiting for 3 seconds");
-    sx_coro_wait(ctx, 3000);
+    sx_coro_wait(d->ctx, 3000);
     puts("fiber_wait_fn: Continue");
 
     puts("fiber_wait_fn: End");
-    sx_coro_end(ctx);    // Always call this !
+    sx_coro_end(d->ctx);    // Always call this !
 }
 
 sx_coro_declare(yield_test)
 {
-    sx_coro_context* ctx = sx_coro_userdata();
+    coro_data* d = sx_coro_userdata();
     puts("fiber_pass_fn: Start");
 
     puts("fiber_pass_fn: Pass and continue on next frame ..");
-    sx_coro_yield(ctx);
+    sx_coro_yield(d->ctx);
     puts("fiber_pass_fn: Continue");
 
     puts("fiber_pass_fn: Pass and continue on next 100 frames ..");
-    sx_coro_yieldn(ctx, 100);
-    puts("fiber_pass_fn: Continue");
+    sx_coro_yieldn(d->ctx, 100);
+
+    puts("fiber_pass_fn: Continue next 10 frames");
+    int i = 0;
+    while (i++ < 10) {
+        printf("fiber_pass_fn: Continue frame: %i\n", d->frame);
+        sx_coro_yield(d->ctx);
+    }
 
     puts("fiber_pass_fn: End");
-    sx_coro_end(ctx);    // Always call this !
+    sx_coro_end(d->ctx);    // Always call this !
 }
 
 
@@ -68,14 +79,17 @@ int main(int argc, char* argv[])
     puts("Fiber context test: ");
     puts("Press CTRL+C to stop");
     sx_coro_context* ctx = sx_coro_create_context(alloc, 10, 32 * 1024);
-
+    coro_data d = {
+        .ctx = ctx,
+        .frame = 0,
+    };
     // Invoke fibers
-    sx_coro_invoke(ctx, wait_test, ctx);
-    sx_coro_invoke(ctx, yield_test, ctx);
-    uint64_t tick = sx_tm_now();
+    sx_coro_invoke(ctx, wait_test, &d);
+    sx_coro_invoke(ctx, yield_test, &d);
     float dt = 0;
-
+    uint64_t tick = sx_tm_now();
     while (1) {
+        d.frame++;
         sx_coro_update(ctx, dt);
         sx_os_sleep(10);
         dt = (float)sx_tm_sec(sx_tm_laptime(&tick));
